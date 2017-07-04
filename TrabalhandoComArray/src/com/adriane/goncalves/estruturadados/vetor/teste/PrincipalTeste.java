149f56697dcc60ef7276e2aaa8227e66cc069a02
root <root@adriane>
2017-07-04 13:50:30 -0300

    Método busca sequencial

--- TrabalhandoComArray/src/com/adriane/goncalves/estruturadados/vetor/teste/PrincipalTeste.java
+++ TrabalhandoComArray/src/com/adriane/goncalves/estruturadados/vetor/teste/PrincipalTeste.java
@@ -8,9 +8,11 @@ public class PrincipalTeste {
 		Vetor v = new Vetor(2);
 		
 		try {
-			v.adiciona("Adriane");
-			v.adiciona("Thalia");
-			System.out.println(v.buscar(1));
+			v.adiciona("elemento 1"); // elemento 1 adicionado na posicao 0
+			v.adiciona("elemento 2"); // elemento 2 adicionado na posicao 1
+			System.out.println(v.buscar(1)); //Verifica se existe elemento na posicao 1 e retorna o conteudo
+			System.out.println(v.Contem("elemento 1")); //Verifica se contém o elemento no vetor
+			System.out.println(v.Contem("elemento 4")); //Verifica se contém o elemento no vetor
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
