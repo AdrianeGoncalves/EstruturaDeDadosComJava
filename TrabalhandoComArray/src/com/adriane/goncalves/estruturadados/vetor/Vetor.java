828eb94db53478314f6e1962e4ebe995936816a4
root <root@adriane>
2017-07-04 11:37:10 -0300

    Criando a classe vetor e adicionando a capacidade

---
+++ TrabalhandoComArray/src/com/adriane/goncalves/estruturadados/vetor/Vetor.java
@@ -0,0 +1,11 @@
+package com.adriane.goncalves.estruturadados.vetor;
+
+public class Vetor {
+	
+	String[] elementos;
+	
+	public Vetor(int capacidade){
+		this.elementos = new String[capacidade];
+	}
+
+}
---
+++ TrabalhandoComArray/src/com/adriane/goncalves/estruturadados/vetor/teste/PrincipalTeste.java
@@ -0,0 +1,12 @@
+package com.adriane.goncalves.estruturadados.vetor.teste;
+
+import com.adriane.goncalves.estruturadados.vetor.Vetor;
+
+public class PrincipalTeste {
+	public static void main(String[]args){
+		
+		Vetor v = new Vetor(5);
+		int i = 0;
+		i++;
+	}
+}
